81ea04c1b19ceb4180f8ac0432a10358b22ccd9b
Homura <egzozu.be1.bas@gmail.com>
2025-08-16 17:25:28 +0300

    Revert "update androidtools"
    This reverts commit 74f8281844015905c7e75f2bd2e17e1d6f7388c4.

--- src/android/Permissions.hx
+++ src/android/Permissions.hx
@@ -16,38 +16,28 @@ class Permissions
 {
 	/**
 	 * Retrieves the list of permissions granted to the application.
-	 * 
+	 *
 	 * @return An array of granted permissions.
 	 */
 	public static inline function getGrantedPermissions():Array<String>
 	{
-		final getGrantedPermissionsJNI:Null<Dynamic> = JNICache.createStaticMethod('org/haxe/extension/Tools', 'requestPermissions', '([Ljava/lang/String;I)V');
-
-		return getGrantedPermissionsJNI != null ? getGrantedPermissionsJNI() : [];
+		return JNICache.createStaticMethod('org/haxe/extension/Tools', 'getGrantedPermissions', '()[Ljava/lang/String;')();
 	}
 
 	/**
 	 * Requests a specific permission from the user via a dialog.
-	 * 
+	 *
 	 * @param permissions The permissions to request. This should be in the format ['android.permission.PERMISSION_NAME'].
 	 * @param requestCode The request code to associate with this permission request.
 	 */
 	public static inline function requestPermissions(permissions:Array<String>, requestCode:Int = 1):Void
 	{
-		final requestPermissionsJNI:Null<Dynamic> = JNICache.createStaticMethod('org/haxe/extension/Tools', 'requestPermissions', '([Ljava/lang/String;I)V');
+		for (i in 0...permissions.length)
+			if (!permissions[i].startsWith('android.permission.'))
+				permissions[i] = 'android.permission.${permissions[i]}';
 
-		if (requestPermissionsJNI != null)
-		{
-			final nativePermissions:Array<String> = [];
-
-			for (i in 0...permissions.length)
-			{
-				if (!permissions[i].startsWith('android.permission.'))
-					nativePermissions[i] = 'android.permission.${permissions[i]}';
-			}
-
-			requestPermissionsJNI(nativePermissions, requestCode);
-		}
+		JNICache.createStaticMethod('org/haxe/extension/Tools', 'requestPermissions',
+			'([Ljava/lang/String;I)V')(permissions, requestCode);
 	}
 }
 #end
