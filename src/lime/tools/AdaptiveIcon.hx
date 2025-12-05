49be153eafa9ed6fdc600e9d50e00841d1864d12
Furo <furogaming9@gmail.com>
2025-12-05 20:54:33 +0100

    Adaptive Icons on `iOS`

--- src/lime/tools/AdaptiveIcon.hx
+++ src/lime/tools/AdaptiveIcon.hx
@@ -1,14 +1,25 @@
 package lime.tools;
 
+import sys.FileSystem;
+import haxe.io.Path;
+
+/**
+	This file is a wrapper to save to path and some properties about the AdaptiveIcon format from Android
+	It is also used to store Apple's Icon Composer .icon file for iOS since its also a dynamic icon file
+**/
 class AdaptiveIcon
 {
 	public var path:String;
-	public var hasRoundIcon:Bool;
+	public var hasRoundIcon:Bool; // No use on iOS
+	public var iconComposerFile:Bool; // No use on Android
 
 	public function new(path:String, hasRoundIcon:Bool)
 	{
 		this.path = path;
 		this.hasRoundIcon = hasRoundIcon;
+
+		// macOS files are actually folders with a file extension
+		this.iconComposerFile = Path.extension(path) == "icon" && FileSystem.isDirectory(path);
 	}
 
 	public function clone():AdaptiveIcon
