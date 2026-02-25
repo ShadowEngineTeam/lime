e46cc727f744c2e11966dfdbfc507afb986da32e
Homura <egzozu.be.bas@gmail.com>
2026-02-25 11:42:05 +0300

    temporary update shadowed Timer

--- src/haxe/Timer.hx
+++ src/haxe/Timer.hx
@@ -1,10 +1,5 @@
-package haxe;
-
-#if (!lime_cffi || macro)
-// Original haxe.Timer class
-
 /*
- * Copyright (C)2005-2018 Haxe Foundation
+ * Copyright (C)2005-2019 Haxe Foundation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -24,28 +19,42 @@ package haxe;
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
+
+package haxe;
+
+#if (!lime_cffi || macro)
+// Original haxe.Timer class
+#if (target.threaded && !cppia)
+import sys.thread.Thread;
+import sys.thread.EventLoop;
+#end
+
 /**
-	The Timer class allows you to create asynchronous timers on platforms that
+	The `Timer` class allows you to create asynchronous timers on platforms that
 	support events.
 
-	The intended usage is to create an instance of the Timer class with a given
-	interval, set its run() method to a custom function to be invoked and
-	eventually call stop() to stop the Timer.
+	The intended usage is to create an instance of the `Timer` class with a given
+	interval, set its `run()` method to a custom function to be invoked and
+	eventually call `stop()` to stop the `Timer`.
 
-	Note that a running Timer may or may not prevent the program to exit
-	automatically when main() returns.
+	Note that a running `Timer` may or may not prevent the program to exit
+	automatically when `main()` returns.
 
-	It is also possible to extend this class and override its run() method in
+	It is also possible to extend this class and override its `run()` method in
 	the child class.
+
+	Notice for threaded targets:
+	`Timer` instances require threads they were created in to run with Haxe's event loops.
+	Main thread of a Haxe program always contains an event loop. For other cases use 
+	`sys.thread.Thread.createWithEventLoop` and `sys.thread.Thread.runWithEventLoop` methods.
 **/
-class Timer
-{
+class Timer {
 	#if (flash || js)
 	private var id:Null<Int>;
-	#elseif java
-	private var timer:java.util.Timer;
-	private var task:java.util.TimerTask;
-	#elseif (haxe_ver >= "3.4.0")
+	#elseif (target.threaded && !cppia)
+	var thread:Thread;
+	var eventHandler:EventHandler;
+	#else
 	private var event:MainLoop.MainEvent;
 	#end
 
@@ -60,24 +69,21 @@ class Timer
 
 		The accuracy of this may be platform-dependent.
 	**/
-	public function new(time_ms:Int)
-	{
+	public function new(time_ms:Int) {
 		#if flash
 		var me = this;
-		id = untyped __global__["flash.utils.setInterval"](function()
-		{
+		id = untyped __global__["flash.utils.setInterval"](function() {
 			me.run();
 		}, time_ms);
 		#elseif js
 		var me = this;
 		id = untyped setInterval(function() me.run(), time_ms);
-		#elseif java
-		timer = new java.util.Timer();
-		timer.scheduleAtFixedRate(task = new TimerTask(this), haxe.Int64.ofInt(time_ms), haxe.Int64.ofInt(time_ms));
-		#elseif (haxe_ver >= "3.4.0")
+		#elseif (target.threaded && !cppia)
+		thread = Thread.current();
+		eventHandler = thread.events.repeat(() -> this.run(), time_ms);
+		#else
 		var dt = time_ms / 1000;
-		event = MainLoop.add(function()
-		{
+		event = MainLoop.add(function() {
 			@:privateAccess event.nextRun += dt;
 			run();
 		});
@@ -93,26 +99,20 @@ class Timer
 
 		It is not possible to restart `this` Timer once stopped.
 	**/
-	public function stop()
-	{
+	public function stop() {
 		#if (flash || js)
-		if (id == null) return;
+		if (id == null)
+			return;
 		#if flash
 		untyped __global__["flash.utils.clearInterval"](id);
 		#elseif js
 		untyped clearInterval(id);
 		#end
 		id = null;
-		#elseif java
-		if (timer != null)
-		{
-			timer.cancel();
-			timer = null;
-		}
-		task = null;
-		#elseif (haxe_ver >= "3.4.0")
-		if (event != null)
-		{
+		#elseif (target.threaded && !cppia)
+		thread.events.cancel(eventHandler);
+		#else
+		if (event != null) {
 			event.stop();
 			event = null;
 		}
@@ -124,8 +124,11 @@ class Timer
 
 		It can be overridden in a subclass, or rebound directly to a custom
 		function:
-			var timer = new haxe.Timer(1000); // 1000ms delay
-			timer.run = function() { ... }
+
+		```haxe
+		var timer = new haxe.Timer(1000); // 1000ms delay
+		timer.run = function() { ... }
+		```
 
 		Once bound, it can still be rebound to different functions until `this`
 		Timer is stopped through a call to `this.stop`.
@@ -136,16 +139,14 @@ class Timer
 		Invokes `f` after `time_ms` milliseconds.
 
 		This is a convenience function for creating a new Timer instance with
-		`time_ms` as argument, binding its run() method to `f` and then stopping
+		`time_ms` as argument, binding its `run()` method to `f` and then stopping
 		`this` Timer upon the first invocation.
 
-		If `f` is null, the result is unspecified.
+		If `f` is `null`, the result is unspecified.
 	**/
-	public static function delay(f:Void->Void, time_ms:Int)
-	{
+	public static function delay(f:Void->Void, time_ms:Int) {
 		var t = new haxe.Timer(time_ms);
-		t.run = function()
-		{
+		t.run = function() {
 			t.stop();
 			f();
 		};
@@ -156,15 +157,14 @@ class Timer
 		Measures the time it takes to execute `f`, in seconds with fractions.
 
 		This is a convenience function for calculating the difference between
-		Timer.stamp() before and after the invocation of `f`.
+		`Timer.stamp()` before and after the invocation of `f`.
 
-		The difference is passed as argument to Log.trace(), with "s" appended
+		The difference is passed as argument to `Log.trace()`, with `"s"` appended
 		to denote the unit. The optional `pos` argument is passed through.
 
-		If `f` is null, the result is unspecified.
+		If `f` is `null`, the result is unspecified.
 	**/
-	public static function measure<T>(f:Void->T, ?pos:PosInfos):T
-	{
+	public static function measure<T>(f:Void->T, ?pos:PosInfos):T {
 		var t0 = stamp();
 		var r = f();
 		Log.trace((stamp() - t0) + "s", pos);
@@ -177,14 +177,16 @@ class Timer
 		The value itself might differ depending on platforms, only differences
 		between two values make sense.
 	**/
-	public static inline function stamp():Float
-	{
+	public static inline function stamp():Float {
 		#if flash
 		return flash.Lib.getTimer() / 1000;
-		#elseif (neko || php)
-		return Sys.time();
 		#elseif js
-		return Date.now().getTime() / 1000;
+		#if nodejs
+		var hrtime = js.Syntax.code('process.hrtime()'); // [seconds, remaining nanoseconds]
+		return hrtime[0] + hrtime[1] / 1e9;
+		#else
+		return @:privateAccess HxOverrides.now() / 1000;
+		#end
 		#elseif cpp
 		return untyped __global__.__time_stamp();
 		#elseif python
@@ -196,25 +198,6 @@ class Timer
 		#end
 	}
 }
-
-#if java
-@:nativeGen
-private class TimerTask extends java.util.TimerTask
-{
-	var timer:Timer;
-
-	public function new(timer:Timer):Void
-	{
-		super();
-		this.timer = timer;
-	}
-
-	@:overload override public function run():Void
-	{
-		timer.run();
-	}
-}
-#end
 #else
 import lime.system.System;
 
