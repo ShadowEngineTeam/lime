0c3102866df15b7856a3dc204d76793beaa0f081
MAJigsaw77 <77043862+MAJigsaw77@users.noreply.github.com>
2026-04-11 22:55:48 +0300

    Refactor `Bytes` allocation logic

--- project/src/utils/Bytes.cpp
+++ project/src/utils/Bytes.cpp
@@ -7,8 +7,6 @@
 namespace lime {
 
 
-	static int id_b;
-	static int id_length;
 	static bool init = false;
 	static bool useBuffer = false;
 	static std::map<Bytes*, bool> hadValue;
@@ -19,9 +17,6 @@ namespace lime {
 	inline void _initializeBytes () {
 
 		if (!init) {
-
-			id_b = val_id ("b");
-			id_length = val_id ("length");
 
 			buffer _buffer = alloc_buffer_len (1);
 
@@ -98,13 +93,16 @@ namespace lime {
 		}
 
 		lime::fseek (file, 0, SEEK_END);
+
 		int size = lime::ftell (file);
+
 		lime::fseek (file, 0, SEEK_SET);
 
 		if (size > 0) {
 
 			Resize (size);
-			int status = lime::fread (b, 1, size, file);
+
+			lime::fread (b, 1, size, file);
 
 		}
 
@@ -136,108 +134,98 @@ namespace lime {
 
 	void Bytes::Resize (int size) {
 
-		if (size != length || (length > 0 && !b)) {
+		if (size == length) {
 
-			mutex.Lock ();
+			return;
 
-			if (size <= 0) {
+		}
 
-				if (b) {
+		mutex.Lock ();
 
-					if (usingValue.find (this) != usingValue.end ()) {
+		if (size <= 0) {
 
-						usingValue.erase (this);
+			if (b) {
 
-					} else {
+				if (usingValue.find (this) == usingValue.end ()) {
 
-						free (b);
-
-					}
-
-					b = 0;
-					length = 0;
+					free (b);
 
 				}
 
-			} else {
-
-				unsigned char* data = (unsigned char*)malloc (sizeof (char) * size);
-
-				if (b) {
-
-					if (length) {
-
-						memcpy (data, b, length < size ? length : size);
-
-					}
-
-					if (usingValue.find (this) != usingValue.end ()) {
-
-						usingValue.erase (this);
-
-					} else {
-
-						free (b);
-
-					}
-
-				} else if (usingValue.find (this) != usingValue.end ()) {
-
-					usingValue.erase (this);
-
-				}
-
-				b = data;
-				length = size;
+				b = 0;
+				length = 0;
 
 			}
 
-			mutex.Unlock ();
+		} else {
+
+			unsigned char* data = (unsigned char*)malloc (sizeof (char) * size);
+
+			if (b && length > 0) {
+
+				memcpy (data, b, length < size ? length : size);
+
+				if (usingValue.find (this) == usingValue.end ()) {
+
+					free (b);
+
+				}
+
+			}
+
+			usingValue.erase (this);
+			b = data;
+			length = size;
 
 		}
+
+		mutex.Unlock ();
 
 	}
 
 
-	void Bytes::Set(value bytes) {	
-		
+	void Bytes::Set(value bytes) {
+
 	    int newLength = 0;
 	    unsigned char* newB = 0;
-	    bool isNull = val_is_null(bytes);
-	
+	    bool isNull = val_is_null (bytes);
+
 	    if (!isNull) {
-	
-	        //here we can extract the values before calling our mutex to avoid potential deadlock or contention
-	        value lengthVal = val_field(bytes, id_length);
-	        value bVal = val_field(bytes, id_b);
-	
+
+	        value lengthVal = val_field (bytes, val_id ("length"));
+	        value bVal = val_field (bytes, val_id ("b"));
+
 	        newLength = val_int(lengthVal);
-	
+
 	        if (newLength > 0) {
-	
-	            if (val_is_string(bVal)) {
-	                newB = (unsigned char*)val_string(bVal);
+
+	            if (val_is_string (bVal)) {
+	                newB = (unsigned char*)val_string (bVal);
 	            } else {
-	                newB = (unsigned char*)buffer_data(val_to_buffer(bVal));
+	                newB = (unsigned char*)buffer_data (val_to_buffer (bVal));
 	            }
 	        }
 	    }
-	
-	    //and now it should be save to lock
-	    mutex.Lock();
-	
+
+	    mutex.Lock ();
+
 	    if (isNull) {
-	        usingValue.erase(this);
+
+	        usingValue.erase (this);
 	        length = 0;
 	        b = 0;
+
 	    } else {
+
 	        hadValue[this] = true;
 	        usingValue[this] = true;
 	        length = newLength;
 	        b = newB;
+
 	    }
-	
-	    mutex.Unlock();
+
+	    mutex.Unlock ();
+
 	}
 
 
@@ -248,6 +236,7 @@ namespace lime {
 		if (size > 0) {
 
 			Resize (size);
+
 			memcpy (b, &data[0], length);
 
 		} else {
@@ -260,10 +249,16 @@ namespace lime {
 
 			}
 
-			mutex.Unlock ();
+			if (b) {
+
+				free (b);
+
+			}
 
 			b = 0;
 			length = 0;
+
+			mutex.Unlock ();
 
 		}
 
@@ -285,38 +280,34 @@ namespace lime {
 
 		} else {
 
-			alloc_field (bytes, id_length, alloc_int (length));
+			alloc_field (bytes, val_id ("length"), alloc_int (length));
 
 			if (useBuffer) {
 
-				value _buffer = val_field (bytes, id_b);
+				value _buffer = val_field (bytes, val_id ("b"));
 
 				if (val_is_null (_buffer) || (char*)b != buffer_data (val_to_buffer (_buffer))) {
 
 					buffer bufferValue = alloc_buffer_len (length);
 					_buffer = buffer_val (bufferValue);
 					memcpy ((unsigned char*)buffer_data (bufferValue), b, length);
-					alloc_field (bytes, id_b, _buffer);
+					alloc_field (bytes, val_id ("b"), _buffer);
 
 				}
 
 			} else {
 
-				value _string = val_field (bytes, id_b);
+				value _string = val_field (bytes, val_id ("b"));
 
 				if (val_is_null (_string) || (const char*)b != val_string (_string)) {
 
 					value data = alloc_raw_string (length);
 					memcpy ((void*)val_string (data), b, length);
-					alloc_field (bytes, id_b, data);
+					alloc_field (bytes, val_id ("b"), data);
 
 				}
 
 			}
-
-			mutex.Lock ();
-			hadValue[this] = true;
-			mutex.Unlock ();
 
 			return bytes;
 
