aeaf2ebfc5911f093d005137e6d57bad3b0f96a0
Homura <egzozu.be.bas@gmail.com>
2026-04-15 21:00:22 +0300

    shrug

--- project/lib/custom/sdl/include/SDL_build_config.h
+++ project/lib/custom/sdl/include/SDL_build_config.h
@@ -499,6 +499,7 @@
 
 # define SDL_VIDEO_DRIVER_UIKIT 1    /**< UIKit video driver */
 # define SDL_VIDEO_METAL 1           /**< Metal support */
+# define SDL_VIDEO_OPENGL_ES 0       /**< OpenGL ES support */
 # define SDL_VIDEO_OPENGL_ES2 0      /**< OpenGL ES 2.0 support */
 /** @} */
 
