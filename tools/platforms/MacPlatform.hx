391f749351489ffaf48b3dc45bb9aa298a3e31cb
Homura <egzozu.be.bas@gmail.com>
2026-04-15 21:02:19 +0300

    nuh uh dirSuffix

--- tools/platforms/MacPlatform.hx
+++ tools/platforms/MacPlatform.hx
@@ -166,7 +166,7 @@ class MacPlatform extends PlatformTarget
 			}
 			else
 			{
-				copyIfNewer(Path.combine(dependency.path, "Mac" + dirSuffix + "/" + dependency.name + ".dylib"), executableDirectory + "/" + dependency.name + ".dylib");
+				copyIfNewer(Path.combine(dependency.path, "Mac/" + dependency.name + ".dylib"), executableDirectory + "/" + dependency.name + ".dylib");
 			}
 		}
 
