74b06ba031d567352a32b7609171f3cd5ab6fa15
Homura <egzozu.be.bas@gmail.com>
2026-06-09 18:06:37 +0300

    let's try it again

--- project/lib/custom/sdl/include/SDL_build_config.h
+++ project/lib/custom/sdl/include/SDL_build_config.h
@@ -467,6 +467,7 @@
 # define SDL_POWER_MACOSX 1              /**< macOS power management */
 # define SDL_SENSOR_DUMMY 1              /**< Dummy sensor driver (no native support) */
 # define SDL_VIDEO_DRIVER_COCOA 1        /**< Cocoa video driver */
+# define SDL_VIDEO_METAL 1               /**< Metal support */
 # define SDL_VIDEO_OPENGL 1              /**< OpenGL support */
 # define SDL_VIDEO_OPENGL_EGL 1          /**< EGL (Embedded OpenGL) support */
 # define SDL_VIDEO_OPENGL_ES2 1          /**< OpenGL ES 2.0 support */
@@ -500,6 +501,7 @@
 # endif
 
 # define SDL_VIDEO_DRIVER_UIKIT 1    /**< UIKit video driver */
+# define SDL_VIDEO_METAL 1           /**< Metal support */
 # define SDL_VIDEO_OPENGL_ES 1       /**< OpenGL ES support */
 # define SDL_VIDEO_OPENGL_ES2 1      /**< OpenGL ES 2.0 support */
 /** @} */
