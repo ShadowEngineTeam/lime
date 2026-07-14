48e5b5b5b5f3a33fbc91d924c00602f540f17027
Homura <egzozu.be.bas@gmail.com>
2026-07-14 17:56:29 +0300

    jesus christ

--- src/lime/graphics/opengl/ext/ARB_texture_compression_rgtc.hx
+++ src/lime/graphics/opengl/ext/ARB_texture_compression_rgtc.hx
@@ -3,6 +3,10 @@ package lime.graphics.opengl.ext;
 @:keep
 @:noCompletion class ARB_texture_compression_rgtc
 {
+	public var COMPRESSED_RED_RGTC1_ARB = 0x8DBB;
+	public var COMPRESSED_SIGNED_RED_RGTC1_ARB = 0x8DBC;
+	public var COMPRESSED_RED_GREEN_RGTC2_ARB = 0x8DBD;
+	public var COMPRESSED_SIGNED_RED_GREEN_RGTC2_ARB = 0x8DBE;
 	public var COMPRESSED_TEXTURE_FORMATS_ARB = 0x86A3;
 	public var NUM_COMPRESSED_TEXTURE_FORMATS_ARB = 0x86A2;
 	public var TEXTURE_COMPRESSED_ARB = 0x86A1;
