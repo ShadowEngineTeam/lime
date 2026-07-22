bc38551fb6b1397f32056a21542d7d2148ee8c64
Homura <egzozu.be.bas@gmail.com>
2026-07-22 15:11:50 +0300

    oops

--- project/lib/openal/alc/backends/ios_audio_session.h
+++ project/lib/openal/alc/backends/ios_audio_session.h
@@ -2,9 +2,12 @@
 
 #ifdef __cplusplus
 extern "C" {
+#else
+#include <stdbool.h>
 #endif
 void OpenAL_IOSAudioSessionInitialize(void);
 void OpenAL_IOSAudioSessionSetActive(bool active);
+void OpenAL_SetIOSAudioUnitActive(bool active);
 #ifdef __cplusplus
 }
 #endif
