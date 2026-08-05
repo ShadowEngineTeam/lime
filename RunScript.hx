5ed05b50ae18975d2a15e53de61c0ccdef9331ad
Homura <egzozu.be.bas@gmail.com>
2026-08-05 09:27:35 +0300

    omg jigsaw bruh

--- RunScript.hx
+++ RunScript.hx
@@ -1,13 +1,16 @@
 package;
 
-import hxp.*;
+import hxp.Haxelib;
+import hxp.Log;
+import hxp.Path;
+import hxp.System;
 import sys.FileSystem;
 
 class RunScript
 {
 	private static function rebuildTools(limeDirectory:String, toolsDirectory:String):Void
 	{
-		System.runCommand(toolsDirectory, "haxe", ["run.hxml"]);
+		System.runCommand(limeDirectory, "haxe", ["run.hxml"]);
 		System.runCommand(toolsDirectory, "haxe", ["tools.hxml"]);
 	}
 
@@ -29,7 +32,7 @@ class RunScript
 			}
 		}
 
-		var result:Dynamic = Sys.command(command, args);
+		final result:Int = Sys.command(command, args);
 
 		if (oldPath != "")
 		{
@@ -46,11 +49,11 @@ class RunScript
 
 	public static function main()
 	{
-		var args = Sys.args();
+		final args:Array<String> = Sys.args();
 
 		if (args.length > 0)
 		{
-			var lastArgument = new Path(args[args.length - 1]).toString();
+			var lastArgument:String = new Path(args[args.length - 1]).toString();
 
 			if (((StringTools.endsWith(lastArgument, "/") && lastArgument != "/") || StringTools.endsWith(lastArgument, "\\"))
 				&& !StringTools.endsWith(lastArgument, ":\\"))
@@ -64,8 +67,8 @@ class RunScript
 			}
 		}
 
-		var limeDirectory = Haxelib.getPath(new Haxelib("lime"), true);
-		var toolsDirectory = Path.combine(limeDirectory, "tools");
+		var limeDirectory:String = Haxelib.getPath(new Haxelib("lime"), true);
+		var toolsDirectory:String = Path.combine(limeDirectory, "tools");
 
 		if (!FileSystem.exists(toolsDirectory))
 		{
@@ -75,23 +78,22 @@ class RunScript
 
 		if (args.length > 2 && args[0] == "rebuild" && args[1] == "tools")
 		{
-			var cacheDirectory = Sys.getCwd();
+			final cacheDirectory:String = Sys.getCwd();
 
 			Sys.setCwd(Haxelib.workingDirectory);
 
 			for (arg in args)
 			{
-				var equals = arg.indexOf("=");
+				final equals:Int = arg.indexOf("=");
 
 				if (equals > -1 && StringTools.startsWith(arg, "--"))
 				{
-					var argValue = arg.substr(equals + 1);
-					var field = arg.substr(2, equals - 2);
+					final argValue:String = arg.substr(equals + 1);
+					final field:String = arg.substr(2, equals - 2);
 
 					if (StringTools.startsWith(field, "haxelib-"))
 					{
-						var name = field.substr(8);
-						Haxelib.pathOverrides.set(name, Path.tryFullPath(argValue));
+						Haxelib.pathOverrides.set(field.substr(8), Path.tryFullPath(argValue));
 					}
 				}
 				else if (StringTools.startsWith(arg, "-"))
@@ -121,7 +123,7 @@ class RunScript
 			}
 		}
 
-		var tools_n = Path.combine(toolsDirectory, "tools.n");
+		final tools_n:String = Path.combine(toolsDirectory, "tools.n");
 
 		if (!FileSystem.exists(tools_n) || args.indexOf("-rebuild") > -1)
 		{
